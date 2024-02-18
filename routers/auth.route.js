5fe46a498d7d0de62e3cfd0c572efbc151ea8053
TheWhitcher <zwhitcher1995@gmail.com>
2024-02-18 14:23:58 -0500

    Adjusted existing database calls to use the Controller methods instead.

--- routers/auth.route.js
+++ routers/auth.route.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const validation = require('../utils/validation.utils');
+const { isEmailUnique, registerUser, loginUser } = require('../database/controllers/userController');
 
 const router = express.Router();
 
@@ -25,10 +26,12 @@ router.post('/register', async (req, res) => {
         res.status(400).res.send({message: verifiedPassword.message});
         return
     };
-
-    const user = await userCollection.findOne({email: body.email});
     
-    if (user){
+    // TODO: Add a verification that First name exists
+    // TODO: Add a verification that Last name exists
+
+    if (!(await isEmailUnique(body.email))){
+        // TODO: If needed adjust failure logic.
         res.status(409);
         res.send({
             message: 'Email already in use.'
@@ -39,18 +42,23 @@ router.post('/register', async (req, res) => {
     const salt = await bcrypt.genSalt(10);
     const passwordHash = await bcrypt.hash(body.password, salt);
 
-    const result = await userCollection.insertOne({
+    const userInfo = {
         firstName: body.firstName,
         lastName: body.lastName,
-        municipality: municipality,
+        municipality: body.municipality,
         email: body.email,
         password: passwordHash,
-    });
+        interestedTags: body.interestedTags
+    }
 
-    res.status(201).send({
-        message: "Success.",
-        result
-    });
+    if (await registerUser(userInfo)){
+        // TODO: If needed adjust success logic.
+        res.status(201).send({
+            message: "User succesfully created.",
+        });
+    } else {
+        // TODO: Enter failure logic.
+    }
 });
 
 router.post('/login', async (req, res) => {
@@ -70,14 +78,20 @@ router.post('/login', async (req, res) => {
         return
     }
 
-    const verify = await bcrypt.compare(body.password, passwordHash);
+    if(await loginUser(body.email, body.password)){
+        // TODO: If needed adjust success logic.
+        res.status(200);
+        res.send({
+            message: "Success.",
+            body: body,
+            verify
+        });
 
-    res.status(200);
-    res.send({
-        message: "Success.",
-        body: body,
-        verify
-    });
+    }else{
+        // TODO: Enter failed login logic.
+    }
+
+
 });
 
 module.exports = router;
