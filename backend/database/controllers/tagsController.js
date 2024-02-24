b16a3ca418bfcb7591886ac51343bd36f3cbf3cc
TheWhitcher <zwhitcher1995@gmail.com>
2024-02-24 14:48:54 -0500

    Created functions for tagsController

--- backend/database/controllers/tagsController.js
+++ backend/database/controllers/tagsController.js
@@ -1,8 +1,64 @@
 const Tag = require('../models/tagsModel');
+// TODO: Create documentation
 
-// TODO: Create a function to add tags to the database
-// TODO: Create a function to get all the tags from the database
-// TODO: Create a function to delete tags from the database
-// TODO: Create a function to modify tags from the database
+async function getAllTags(){
+    try {
+        const tags = await Tag.find({}).toArray();
+        return tags;
 
-module.exports = {  };
+    } catch (error) {
+        console.error('Unexpected error retreiving the list of tags: ', error);
+        throw error;
+    }
+}
+
+async function createOneTag(newTag){
+    try{        
+        const isExisting = await Tag.findOne({ tag: {$regex: newTag, $options: 'i'} });
+
+        if (!isExisting){
+            await Tag.create({ tag: newTag });
+            return true;
+        } else {
+            console.log('Tag already exists.');
+            return false;
+        }
+    } catch (error) {
+        console.error('Unexpected error creating tag: ', error);
+        throw error;
+    }
+}
+
+async function deleteOneTag(deleteTag){
+    try {
+        const result = await Tag.deleteOne({ tag: deleteTag });
+    
+        if (result.deletedCount > 0){
+            return true;
+        } else {
+            console.error('No matching tags to delete.');
+            return false;
+        }
+    } catch (error) {
+        console.error('Unexpected error deleting tag: ', error);
+        throw error;
+    }
+}
+
+async function updateTag(currentTag, updatedTag) {
+    try{
+        const result = await Tag.updateOne({ tag: currentTag }, { $set: { tag: updatedTag }});
+
+        if(result.n > 0){
+            return true;
+        } else {
+            console.error('No matchin tags to update.');
+            return false;
+        }
+    } catch (error) {
+        console.error('Unexpected error updated the tag: ', error);
+        throw error;
+    }
+}
+
+module.exports = { getAllTags, createOneTag, deleteOneTag, updateTag };
