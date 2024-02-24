07c013ed14b5cda3fbcf9c15c9787b4abca13aff
TheWhitcher <zwhitcher1995@gmail.com>
2024-02-24 11:36:35 -0500

    Added TODOs to the database controllers files.

---
+++ backend/database/controllers/mailingListController.js
@@ -0,0 +1,7 @@
+const MailingList = require('../models/mailingListModel');
+
+// TODO: Create a function that updates the mailing list 
+// TODO: Create a function the gets all the users from a specified tag
+// TODO: Create a function that deletes a document in the database.
+
+module.exports = { };
---
+++ backend/database/controllers/tagsController.js
@@ -0,0 +1,8 @@
+const Tag = require('../models/tagsModel');
+
+// TODO: Create a function to add tags to the database
+// TODO: Create a function to get all the tags from the database
+// TODO: Create a function to delete tags from the database
+// TODO: Create a function to modify tags from the database
+
+module.exports = {  };
