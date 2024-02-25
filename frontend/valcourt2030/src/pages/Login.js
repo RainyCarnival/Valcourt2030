2ad09aab637eed78e6d35138770fb6cafc1fa018
TheWhitcher <zwhitcher1995@gmail.com>
2024-02-25 01:58:38 -0500

    Added logic and error handling for logging in

--- frontend/valcourt2030/src/pages/Login.js
+++ frontend/valcourt2030/src/pages/Login.js
@@ -1,22 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Form, Button, Card } from 'react-bootstrap';
 import logo from '../logo.png';
 import Background from '../components/Background';
+import axios from 'axios';
+import { loginRoute } from '../utils/APIRoutes';
 
 export default function LoginPage() {
+  const [values, setValues] = useState({ email: "", password: "" });
+  const [error, setError] = useState(null);
 
   const navigate = useNavigate(); // Hook for navigation
+
+  /**
+   * Validates the login form.
+   * @returns {boolean} - Returns true if the form is valid, otherwise false.
+   */
+  const validateForm = () => {
+    const { email, password } = values;
+
+    if (email === "") {
+      setError('* Courriel et mot de passe est requis.')
+      return false;
+    } else if (password === "") {
+      setError('* Courriel et mot de passe est requis.')
+      return false;
+    }
+    return true;
+  };
+
+  /**
+   * Handles the form submission.
+   * Sends a login request to the server and processes the response.
+   * @param {Object} event - The form submission event.
+   */
+  const handleLogin = async (event) => { 
+    event.preventDefault();
+
+    if (validateForm()) {
+      try{
+        const { email, password } = values;
+        const { data } = await axios.post(loginRoute, {
+          email,
+          password,
+        });
   
-  const handleLogin = () => {
-    //Todo: Add login logic
-    //Need to add a check for the user's credentials
-    navigate('/userEvent');
-  }
+        if (data.status) {
+          navigate('/userEvent');
+        }
+
+      } catch (error) {
+        if (axios.isAxiosError(error) && error.response) {
+          const { status } = error.response;
+
+          if (status === 400) {
+            setError('* Identifiants de connexion incorrects. \n\nVeuillez vérifier votre adresse courriel et votre mot de passe.');
+          } else if (status === 500) {
+            setError('* Une erreur interne du serveur s\'est produite. \nVeuillez réessayer plus tard.');
+          } else {
+            setError(`* Erreur inattendue lors de la connexion. \nVeuillez réessayer plus tard.`);
+          }
+        } else {
+          // Handle non-Axios errors
+          setError(`* Une erreur interne du serveur s\'est produite. \nVeuillez réessayer plus tard.`);
+        }
+      }
+    }
+  };
 
   const handleSignUpClick = () => {
     navigate('/signup'); // Navigate to the signup page
+  };
+
+  const handleChange = (event) => {
+    setValues({ ...values, [event.target.name]: event.target.value });
   };
 
   return (
@@ -33,13 +91,17 @@ export default function LoginPage() {
           </Card.Title>
           <Form>
             <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Control type="email" placeholder="Courriel" />
+              <Form.Control type="email" placeholder="Courriel" name="email" onChange={(e) => handleChange(e)} />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Control type="password" placeholder="Mot de passe" />
+              <Form.Control type="password" placeholder="Mot de passe" name="password" onChange={(e) => handleChange(e)} />
             </Form.Group>
-
+            {error && (
+              <div className="alert alert-danger my-3 py-2" role="alert" style={{ fontSize: 'small' }}>
+                {error}
+              </div>
+            )}
             <Button 
               style={{ backgroundColor: 'rgba(0, 152, 217, 0.5)', borderColor: 'rgba(0, 152, 217, 0.5)' }} 
               type="submit" 
