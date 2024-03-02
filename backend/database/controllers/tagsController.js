0277cd5f98c7302a6dabfd902c52dfc440113769
TheWhitcher <zwhitcher1995@gmail.com>
2024-03-02 10:20:04 -0500

    ran lint:fix to fix indentation warnings

--- backend/database/controllers/tagsController.js
+++ backend/database/controllers/tagsController.js
@@ -2,85 +2,85 @@ const Tag = require('../models/tagsModel');
 // TODO Create documentation
 
 async function getOneTag(tagToFind){
-    try {
-        const tag = await Tag.findOne(tagToFind);
+	try {
+		const tag = await Tag.findOne(tagToFind);
 
-        if(tag){
-            return tag;
-        } else {
-            console.error('Tag not found.');
-            return false;
-        }
+		if(tag){
+			return tag;
+		} else {
+			console.error('Tag not found.');
+			return false;
+		}
 
-    } catch (error) {
-        console.error('Unexpected error retreiving the tag: ', error);
-        throw error;
-    }
+	} catch (error) {
+		console.error('Unexpected error retreiving the tag: ', error);
+		throw error;
+	}
 }
 
 async function getAllTags(){
-    try {
-        const tags = await Tag.find({});
+	try {
+		const tags = await Tag.find({});
 
-        if(tags.length === 0){
-            console.warn('No tags found.');
-        }
+		if(tags.length === 0){
+			console.warn('No tags found.');
+		}
 
-        return tags;
+		return tags;
 
-    } catch (error) {
-        console.error('Unexpected error retreiving the list of tags: ', error);
-        throw error;
-    }
+	} catch (error) {
+		console.error('Unexpected error retreiving the list of tags: ', error);
+		throw error;
+	}
 }
 
 async function createOneTag(newTag){
-    try{        
-        const isExisting = await Tag.findOne({ tag: {$regex: newTag, $options: 'i'} });
+	try{        
+		const isExisting = await Tag.findOne({ tag: {$regex: newTag, $options: 'i'} });
 
-        if (!isExisting){
-            await Tag.create({ tag: newTag });
-            return true;
-        } else {
-            console.log('Tag already exists.');
-            return false;
-        }
-    } catch (error) {
-        console.error('Unexpected error creating tag: ', error);
-        throw error;
-    }
+		if (!isExisting){
+			await Tag.create({ tag: newTag });
+			return true;
+		} else {
+			console.log('Tag already exists.');
+			return false;
+		}
+	} catch (error) {
+		console.error('Unexpected error creating tag: ', error);
+		throw error;
+	}
 }
 
 async function deleteOneTag(tagToDelete){
-    try {
-        const result = await Tag.deleteOne({ tag: tagToDelete });
+	try {
+		const result = await Tag.deleteOne({ tag: tagToDelete });
     
-        if (result.deletedCount > 0){
-            return true;
-        } else {
-            console.error('No matching tags to delete.');
-            return false;
-        }
-    } catch (error) {
-        console.error('Unexpected error deleting tag: ', error);
-        throw error;
-    }
+		if (result.deletedCount > 0){
+			return true;
+		} else {
+			console.error('No matching tags to delete.');
+			return false;
+		}
+	} catch (error) {
+		console.error('Unexpected error deleting tag: ', error);
+		throw error;
+	}
 }
 
 async function updateTag(currentTag, tagUpdateData) {
-    try{
-        const result = await Tag.updateOne({ tag: currentTag }, { $set: { tag: tagUpdateData }});
+	try{
+		const result = await Tag.updateOne({ tag: currentTag }, { $set: { tag: tagUpdateData }});
 
-        if(result.n > 0){
-            return true;
-        } else {
-            console.error('No matchin tags to update.');
-            return false;
-        }
-    } catch (error) {
-        console.error('Unexpected error updated the tag: ', error);
-        throw error;
-    }
+		if(result.n > 0){
+			return true;
+		} else {
+			console.error('No matching tags to update.');
+			return false;
+		}
+	} catch (error) {
+		console.error('Unexpected error updated the tag: ', error);
+		throw error;
+	}
 }
 
 module.exports = { getOneTag, getAllTags, createOneTag, deleteOneTag, updateTag };
