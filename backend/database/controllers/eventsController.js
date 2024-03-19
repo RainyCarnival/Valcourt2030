f113b1024bac2965d41cf4e8892ff29a44c46e6e
TheWhitcher <zwhitcher1995@gmail.com>
2024-03-18 21:32:23 -0400

    Created and documented event Model and Controller (Not Tested yet)

--- backend/database/controllers/eventsController.js
+++ backend/database/controllers/eventsController.js
@@ -1,7 +1,160 @@
 const Event = require('../models/eventsModel');
 
-// TODO: Create a function to add new events to tha database.
-// TODO: Create a function to modify events in the database.
-// TODO: Create a function to delete events in the database.
+/**
+ * Retrieves all events from the database.
+ * @returns {Promise<Array<Object>>} A Promise that resolves to an array of event objects.
+ * @throws {Error} If an unexpected error occurs while retrieving events.
+ */
+async function getAllEvents(){
+	try {
+		const events = await Event.find({});
 
-module.exports = { };
+		if(events.length === 0){
+			console.warn('No Events found.');
+			return false;
+		}
+
+		return events;
+
+	} catch (error) {
+		console.error('An unexpected error occured retreiving the list of events: ', error);
+		throw error;
+	}
+}
+
+/**
+ * Retrieves a single event from the database based on the provided eventId.
+ * @param {string} eventId - The ID of the event to retrieve.
+ * @returns {Promise<Object|boolean>} A Promise that resolves to the event object if found, or false if the event is not found.
+ * @throws {Error} If an unexpected error occurs while retrieving the event.
+ */
+async function getOneEvent(eventId){
+	try {
+		const event = await Event.findOne({eventId: eventId});
+
+		if(!event){
+			console.error('Event not found.');
+			return false;
+		}
+
+		return event;
+
+	} catch (error) {
+		console.error('An unexpected error occured retreiving the event: ', error);
+		throw error;
+	}
+}
+
+/**
+ * Creates a new event in the database based on the provided eventData.
+ * @param {Object} eventData - The data for the event to be created.
+ * @returns {Promise<Object|boolean>} A Promise that resolves to the created event object if successful, or false if creation fails.
+ * @throws {Error} If an unexpected error occurs during event creation.
+ */
+async function createOneEvent(eventData){
+	try {
+		const result = await Event.create(eventData);
+
+		if (!result) {
+			throw new Error('Creation Error: Failed to create the event.');
+		}
+
+		return result;
+	} catch (error) {
+		if (error.message.startsWith('Creation Fail')) {
+			console.error(error);
+		} else {
+			console.error('An unexpected error occured creating the event: ', error);
+		}
+
+		return false;
+	}
+}
+
+/**
+ * Updates a single event in the database based on the provided eventId and eventUpdateData.
+ * @param {string} eventId - The ID of the event to be updated.
+ * @param {Object} eventUpdateData - The data to update the event with.
+ * @returns {Promise<Array<string>|boolean>} A Promise that resolves to an array of tags of the updated event if successful, or false if update fails.
+ * @throws {Error} If the event to be updated is not found, if no modifications are made, or if an unexpected error occurs during update.
+ */
+async function updateOneEvent(eventId, eventUpdateData) {
+	try{
+		let allTags;
+
+		const originalEvent = await Event.findOne({eventId: eventId});
+
+		if(!originalEvent){
+			throw new Error(`Document Not Found: No document found for event id ${eventId} to update.`);
+		}
+
+		// Update the event document and retrieve the updated version.
+		const updatedEvent = await Event.findOneAndUpdate({eventId: eventId}, {$set: eventUpdateData}, {new: true});
+
+		// Check if any modifications were made by comparing keys in eventUpdateData
+		const isModified = !Object.keys(eventUpdateData).every(key => {
+			return JSON.stringify(updatedEvent[key]) === JSON.stringify(originalEvent[key]);
+		});
+
+		if(!isModified){
+			throw new Error('Update Error: No modifications to be made, the data is the same.');
+		}
+		
+		// Handle added tags.
+		const addedTags = updatedEvent.tags.filter(tag => !originalEvent.tags.includes(tag));
+
+		if (addedTags.length > 0){
+			allTags = [...originalEvent.tags, ...addedTags];
+		} else {
+			allTags = originalEvent.tags;
+		}
+
+		return allTags;
+
+	} catch (error) {
+
+		if (error.message.startsWith('Document Not Found')){
+			console.error(error);
+		} else if (error.message.startsWith('Update Error')) {
+			console.error(error);
+		} else {
+			console.error('An unexpected error occured when updated user data: ', error);
+		}
+		
+		return false;
+	}
+}
+
+/**
+ * Deletes a single event from the database based on the provided eventId.
+ * @param {string} eventId - The ID of the event to be deleted.
+ * @returns {Promise<Array<string>|boolean>} A Promise that resolves to an array of tags of the deleted event if successful, or false if deletion fails.
+ * @throws {Error} If the event to be deleted is not found or if an unexpected error occurs during deletion.
+ */
+async function deleteOneEvent(eventId){
+	try {
+		const event = await Event.findOne({ eventId: eventId});
+
+		if (!event){
+			throw new Error('Deletion Error: Event not found');
+		}
+
+		const result = await Event.deleteOne({ eventId: eventId });
+
+		if (result.deletedCount <= 0){
+			throw new Error('Deletion Error: Failed to delete event.');
+		}
+
+		return event.tags;
+	} catch (error) {
+		if(error.message.startsWith('Deletion Error')){
+			console.log(error);
+		} else {
+			console.error('An unexpected error occured deleting the event: ', error);
+		}
+
+		return false;
+	}
+}
+
+module.exports = { getAllEvents, getOneEvent, deleteOneEvent, createOneEvent, updateOneEvent};
