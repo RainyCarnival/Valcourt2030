1ceefdb74897fd17e37181b14cec0a0c7b610c8f
TheWhitcher <zwhitcher1995@gmail.com>
2024-04-08 23:56:53 -0400

    live demo set up just need to change the host url in Production environment when ready

--- frontend/valcourt2030/src/utils/APIRoutes.js
+++ frontend/valcourt2030/src/utils/APIRoutes.js
@@ -7,6 +7,8 @@ export const allMunicipalitiesRoute = `${host}/municipalities/getAllMunicipaliti
 
 
 /* PRODUCTION ENVRIONMENT */
-// export const host = '';
-// export const loginRoute = './auth/login';
-// export const registerRoute = './auth/register';
+// export const host = 'https://9cdd-45-88-190-47.ngrok-free.app';
+// export const loginRoute = `${host}/auth/login`;
+// export const registerRoute = `${host}/auth/register`;
+// export const allTagsRoute = `${host}/getAllTags`;
+// export const allMunicipalitiesRoute = `${host}/getAllMunicipalities`;
