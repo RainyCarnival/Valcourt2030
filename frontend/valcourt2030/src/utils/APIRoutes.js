d09d6dffab5f98f7e296d3575e31d8378e121c24
TheWhitcher <zwhitcher1995@gmail.com>
2024-04-09 22:13:57 -0400

    preping dynamic events for home page

--- frontend/valcourt2030/src/utils/APIRoutes.js
+++ frontend/valcourt2030/src/utils/APIRoutes.js
@@ -1,14 +1,16 @@
 /* LOCAL ENVIRONMENT */
-// export const host = "http://localhost:3333";
-// export const loginRoute = `${host}/auth/login`;
-// export const registerRoute = `${host}/auth/register`;
-// export const allTagsRoute = `${host}/getAllTags`;
-// export const allMunicipalitiesRoute = `${host}/getAllMunicipalities`;
-
-
-/* PRODUCTION ENVRIONMENT */
-export const host = 'https://9cdd-45-88-190-47.ngrok-free.app';
+export const host = "http://localhost:3333";
 export const loginRoute = `${host}/auth/login`;
 export const registerRoute = `${host}/auth/register`;
 export const allTagsRoute = `${host}/getAllTags`;
 export const allMunicipalitiesRoute = `${host}/getAllMunicipalities`;
+export const allEventsRoute = `${host}/getAllEvents`;
+
+
+/* PRODUCTION ENVRIONMENT */
+// export const host = 'https://9cdd-45-88-190-47.ngrok-free.app';
+// export const loginRoute = `${host}/auth/login`;
+// export const registerRoute = `${host}/auth/register`;
+// export const allTagsRoute = `${host}/getAllTags`;
+// export const allMunicipalitiesRoute = `${host}/getAllMunicipalities`;
+// export const allEventsRoute = `${host}/getAllEvents`;
