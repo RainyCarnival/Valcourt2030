245b391388d556661892a7992005112abdaf3759
TheWhitcher <zwhitcher1995@gmail.com>
2024-04-09 23:08:37 -0400

    dynamic events on the home page

--- frontend/valcourt2030/src/utils/APIRoutes.js
+++ frontend/valcourt2030/src/utils/APIRoutes.js
@@ -1,14 +1,15 @@
 /* LOCAL ENVIRONMENT */
 export const host = "http://localhost:3333";
-export const loginRoute = `${host}/auth/login`;
-export const registerRoute = `${host}/auth/register`;
-export const allTagsRoute = `${host}/tags/getAllTags`;
-export const allMunicipalitiesRoute = `${host}/municipalities/getAllMunicipalities`;
-
 
 /* PRODUCTION ENVRIONMENT */
 // export const host = 'https://9cdd-45-88-190-47.ngrok-free.app';
-// export const loginRoute = `${host}/auth/login`;
-// export const registerRoute = `${host}/auth/register`;
-// export const allTagsRoute = `${host}/getAllTags`;
-// export const allMunicipalitiesRoute = `${host}/getAllMunicipalities`;
+
+export const loginRoute = `${host}/auth/login`;
+export const registerRoute = `${host}/auth/register`;
+export const allTagsRoute = `${host}/tags/getAllTags`;
+export const createTagRoute = `${host}/tags/createTag`;
+export const allMunicipalitiesRoute = `${host}/municipalities/getAllMunicipalities`;
+export const createMunicipalityRoute = `${host}/municipalities/createMunicipality`;
+export const updateUserRoute = `${host}/users/updateUser`;
+export const deleteUserRoute = `${host}/users/deleteUser`;
+export const allEventsRoute = `${host}/events/getAllEvents`;
